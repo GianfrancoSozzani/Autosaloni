cb94cfc1c79adbf68b9321bb0ecc1045e93affa0
yuliyastepanyuk <84444059+yuliyastepanyuk@users.noreply.github.com>
2025-03-07 17:40:08 +0100

    Sistemato branch devyuliya con i file di yuliyastepanyuk

--- App_Code/DB.cs
+++ App_Code/DB.cs
@@ -1,38 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Data;
 using System.Linq;
 using System.Web;
+using System.Data;
 
-
+/// <summary>
+/// Descrizione di riepilogo per DB
+/// </summary>
 public class DB
 {
-
-
     SqlConnection conn = new SqlConnection();
-    SqlDataAdapter DA = new SqlDataAdapter();
     public SqlCommand cmd = new SqlCommand();
+    SqlDataAdapter DA = new SqlDataAdapter();
+    //DataTable DT = new DataTable();
+
     public string query;
+
     public DB()
     {
-        conn.ConnectionString = "Data Source = DESKTOP-KM2T7UL\\SQLEXPRESS; Initial Catalog=AUTOSALONI; Integrated Security=true";
+        //
+        // TODO: aggiungere qui la logica del costruttore
+        //
+        conn.ConnectionString = "Data Source = DESKTOP-0E2GJI9\\SQLEXPRESS; Initial Catalog=AUTOSALONI; Integrated Security=true;";
         cmd.Connection = conn;
         cmd.CommandType = CommandType.StoredProcedure;
     }
+
     public DataTable SQLselect()
+
     {
         DataTable DT = new DataTable();
+        //DT.Clear();
         cmd.CommandText = query;
         DA.SelectCommand = cmd;
         DA.Fill(DT);
         return DT;
     }
-    public void SQLCommand()
+
+    public void SQLcommand()
     {
+
         cmd.CommandText = query;
+        // parametri
         conn.Open();
         cmd.ExecuteNonQuery();
         conn.Close();
+
+
     }
 }
