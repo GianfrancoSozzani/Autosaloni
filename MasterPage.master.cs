4a173556f6101c69b68b476d0f9e9fd43179fa46
GianfrancoSozzani <gianfranco.sozzani@gmail.com>
2025-03-08 16:17:47 +0100

    correction on comments

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -9,7 +9,7 @@ public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        controllo su utente in session
+        //controllo su utente in session
         if (String.IsNullOrEmpty(Session["USR"] as string))
         {
             Response.Redirect("Login.aspx");
