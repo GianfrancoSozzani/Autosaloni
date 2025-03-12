be7a678b978ef330a945159ce2e2d24e1f040969
Krystian Kamil Grupa <25132033+KriiXOne@users.noreply.github.com>
2025-03-12 18:24:20 +0100

    Set correct parameters in DBConfig.cs
    Parameters set to 705G4DM as this branch is made to run on that machine

--- App_Code/DBConfig.cs
+++ App_Code/DBConfig.cs
@@ -10,7 +10,7 @@ public class DBConfig
     public DBConfig()
 	{
         // Modifica questa stringa con i tuoi parametri locali
-        connectionString = "Data Source=DESKTOP-KM2T7UL\\SQLEXPRESS;Initial Catalog=AUTOSALONI;Integrated Security=True;Encrypt=False";
+        connectionString = "Data Source = DESKTOP-705G4DM\\SQLEXPRESS; Initial Catalog=AUTOSALONI; Integrated Security=true";
     }
 
     public string GetConnectionString()
