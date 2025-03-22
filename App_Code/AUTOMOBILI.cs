fbdfefdf2e1442220874d9d6f1c906c6df943ffd
GianfrancoSozzani <gianfranco.sozzani@gmail.com>
2025-03-22 13:18:23 +0100

    Completata automobili inserimento.
    Aggiunti nuovi controlli:
    
    ora viene controllato che venga inserito necessariamwnte un venditore, un responsabile e un modello, così da evitare problemi in caso di dropdownlist subordinate con 0 item;
    
    ora viene controllato che il prezzo di acquisto e il chilometraggio siano rispettivamnete un decimal o un intero

--- App_Code/AUTOMOBILI.cs
+++ App_Code/AUTOMOBILI.cs
@@ -29,12 +29,12 @@ public class AUTOMOBILI
     public DateTime Data_Vendita;
     public int K_Cliente_Vendita;
     public int K_Marca;
-   
+
     public AUTOMOBILI()
     {
-        
+
     }
-    
+
     //lista
     public DataTable SelectAll()
     {
@@ -43,11 +43,69 @@ public class AUTOMOBILI
         return db.SQLselect();
     }
 
+    //ddl
     public DataTable AUTOMOBILI_ddlModelli()
     {
         DB db = new DB();
         db.query = "AUTOMOBILI_ddlModelli";
         db.cmd.Parameters.AddWithValue("@marca", K_Marca);
         return db.SQLselect();
+    }
+
+    public DataTable AUTOMOBILI_ddlClientiAcquisto()
+    {
+        DB db = new DB();
+        db.query = "AUTOMOBILI_ddlClientiAcquisto";
+        return db.SQLselect();
+    }
+
+    public DataTable AUTOMOBILI_ddlResponabiliSalone()
+    {
+        DB db = new DB();
+        db.query = "AUTOMOBILI_ddlResponsabiliSalone";
+        db.cmd.Parameters.AddWithValue("@salone", K_Salone);
+        return db.SQLselect();
+    }
+
+    public DataTable AUTOMOBILI_ddlVenditoriSalone()
+    {
+        DB db = new DB();
+        db.query = "AUTOMOBILI_ddlVenditoriSalone";
+        db.cmd.Parameters.AddWithValue("@salone", K_Salone);
+        return db.SQLselect();
+    }
+
+    //seleziona_automobile
+
+    public DataTable AUTOMOBILI_SelezionaAutomobile()
+    {
+        DB db = new DB();
+        db.query = "AUTOMOBILI_SelezionaAutomobile";
+        db.cmd.Parameters.AddWithValue("@vin", Telaio);
+        return db.SQLselect();
+    }
+
+    //inserimento
+    public void Inserimento()
+    {
+        DB db = new DB();
+        db.query = "AUTOMOBILI_Inserimento";
+        db.cmd.Parameters.AddWithValue("@modello", K_Modello);
+        db.cmd.Parameters.AddWithValue("@stato", Stato);
+        db.cmd.Parameters.AddWithValue("@data_acquisto", Data_Acquisto);
+        db.cmd.Parameters.AddWithValue("@cliente_acquisto", K_Cliente_Acquisto);
+        db.cmd.Parameters.AddWithValue("@prezzo_acquisto", Prezzo_Acquisto);
+        db.cmd.Parameters.AddWithValue("@salone", K_Salone);
+        db.cmd.Parameters.AddWithValue("@responsabile", K_Responsabile);
+        db.cmd.Parameters.AddWithValue("@venditore", K_Venditore);
+        db.cmd.Parameters.AddWithValue("@alimentazione", Alimentazione);
+        db.cmd.Parameters.AddWithValue("@colore", Colore);
+        db.cmd.Parameters.AddWithValue("@km", KM);
+        db.cmd.Parameters.AddWithValue("@cambio", Cambio);
+        db.cmd.Parameters.AddWithValue("@targa", Targa);
+        db.cmd.Parameters.AddWithValue("@vin", Telaio);
+        db.cmd.Parameters.AddWithValue("@condizione", Condizione);
+        db.cmd.Parameters.AddWithValue("@optional", Optional);
+        db.SQLCommand();
     }
 }
